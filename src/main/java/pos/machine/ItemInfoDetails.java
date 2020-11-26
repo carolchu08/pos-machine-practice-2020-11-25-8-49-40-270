dac50f74c7dc7da8f64dce5fda1e1356699e6c38
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-26 22:52:24 +0800

    refractor code

---
+++ src/main/java/pos/machine/ItemInfoDetails.java
@@ -0,0 +1,40 @@
+package pos.machine;
+
+public class ItemInfoDetails {
+    private final String barcode;
+    private final String name;
+    private final int unitPrice;
+    private int quantity=0;
+    private int subtotal=0;
+
+    public ItemInfoDetails(String barcode, String name, int unitPrice) {
+        this.barcode = barcode;
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        this.subtotal = this.unitPrice * quantity;
+    }
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getSubtotal() {
+        return subtotal;
+    }
+}
+
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,52 @@
 package pos.machine;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PosMachine {
+    static List<ItemInfo> ITEM_INFOS = ItemDataLoader.loadAllItemInfos();
+    private List<ItemInfoDetails> itemInfoDetails;
+    private int total;
+
+
     public String printReceipt(List<String> barcodes) {
-        return null;
+        ArrayList<ItemInfoDetails> itemInfoDetails = getItemInfo(barcodes);
+        calculateSubTotalOfItem(itemInfoDetails);
+        String receipt = "***<store earning no money>Receipt***\n";
+
+        for (ItemInfoDetails itemInfoDetail : this.itemInfoDetails) {
+            receipt += String.format("Name: %s, Quantity: %d, Unit price: %d (yuan), Subtotal: %d (yuan)\n", itemInfoDetail.getName(), itemInfoDetail.getQuantity(), itemInfoDetail.getUnitPrice(), itemInfoDetail.getSubtotal());
+        }
+        receipt += "----------------------\n";
+        receipt += "Total: " + total + " (yuan)\n";
+        receipt += "**********************";
+        return receipt;
     }
+
+
+    public static ArrayList<ItemInfoDetails> getItemInfo(List<String> barcodes) {
+        ArrayList<ItemInfoDetails> itemsInformation = new ArrayList<>();
+        List<String> uniqueBarcodes = barcodes.stream().distinct().collect(Collectors.toList());
+        uniqueBarcodes.forEach(barcode -> itemsInformation.add(retrieveItemDetail(barcode)));
+        return countQuantityOfItems(barcodes, itemsInformation);
+    }
+    private static ItemInfoDetails retrieveItemDetail(String barcode) {
+        ItemInfo item = ITEM_INFOS.stream().filter(ItemInfo -> ItemInfo.getBarcode().equals(barcode)).findFirst().get();
+        return new ItemInfoDetails(item.getBarcode(), item.getName(), item.getPrice());
+    }
+
+
+    private static ArrayList<ItemInfoDetails> countQuantityOfItems(List<String> barcodes, ArrayList<ItemInfoDetails> itemInfoDetails) {
+        itemInfoDetails.forEach(itemInfo -> {
+            itemInfo.setQuantity(Collections.frequency(barcodes, itemInfo.getBarcode()));
+        });
+        return itemInfoDetails;
+    }
+
+    public void calculateSubTotalOfItem(List<ItemInfoDetails> itemInfoDetails) {
+
+        this.itemInfoDetails= itemInfoDetails;
+        total = itemInfoDetails.stream().mapToInt(ItemInfoDetails::getSubtotal).sum();
+    }
+
 }
